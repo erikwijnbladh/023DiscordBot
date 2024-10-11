8393fbce5e140ae16d3ff4ec84e3ac1a2e84cec4
ErikWijnbladh <wijnbladherik@gmail.com>
2024-10-11 17:30:30 +0200

    Keep alive workaround

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
-// bot.js
 const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 const handleCommands = require("./modules/commands");
+const express = require("express");
 
+// Discord client setup
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -20,3 +21,15 @@ client.on("messageCreate", handleCommands);
 
 // Login to Discord
 client.login(process.env.DISCORD_BOT_TOKEN);
+
+// Express server to keep the Replit project alive
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Web server is running on port ${PORT}`);
+});
