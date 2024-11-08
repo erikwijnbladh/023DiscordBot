b582b2daaa4483025912386ba9838383590b69c3
ErikWijnbladh <wijnbladherik@gmail.com>
2024-11-08 07:42:44 +0100

    Data is now Firelands

--- modules/raids.js
+++ modules/raids.js
@@ -1,8 +1,9 @@
 // raids.js
 const raids = [
-  { id: 1020, name: "Throne of the Four Winds" },
-  { id: 1021, name: "Blackwing Descent" },
-  { id: 1031, name: "Bastion of Twilight" },
+  // { id: 1020, name: "Throne of the Four Winds" },
+  // { id: 1021, name: "Blackwing Descent" },
+  // { id: 1031, name: "Bastion of Twilight" },
+  { id: 1027, name: "Firelands" },
 ];
 
 module.exports = raids;
