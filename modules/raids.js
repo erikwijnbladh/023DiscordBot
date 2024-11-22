065a9667d95055a1c536833e8c3c040d4babd3e6
ErikWijnbladh <wijnbladherik@gmail.com>
2024-11-22 08:51:29 +0100

    ID corrected

--- modules/raids.js
+++ modules/raids.js
@@ -3,7 +3,7 @@ const raids = [
   // { id: 1020, name: "Throne of the Four Winds" },
   // { id: 1021, name: "Blackwing Descent" },
   // { id: 1031, name: "Bastion of Twilight" },
-  { id: 1027, name: "Firelands" },
+  { id: 1201, name: "Firelands" },
 ];
 
 module.exports = raids;
