76938a10367f2366bbfe7f6bd1d596346e9dc666
ErikWijnbladh <wijnbladherik@gmail.com>
2025-03-07 13:34:20 +0100

    DS

--- modules/raids.js
+++ modules/raids.js
@@ -3,7 +3,8 @@ const raids = [
   // { id: 1020, name: "Throne of the Four Winds" },
   // { id: 1021, name: "Blackwing Descent" },
   // { id: 1031, name: "Bastion of Twilight" },
-  { id: 1201, name: "Firelands" },
+  // { id: 1201, name: "Firelands" },
+  { id: 1033, name: "Dragon Soul" },
 ];
 
 module.exports = raids;
